4c385d76f0783ceeb5fa5773a548fdebb28e4772
Adriana <adriana.amaral@msn.com>
2021-06-23 17:58:43 -0300

    front-page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-// import logo from './logo.svg';
+// import book from '/public';
 import './App.css';
+import React from 'react'
 
-function App() {
+class App extends React.Component {
+
+  
+
+  render() 
+
+    { 
+   
   return (
     <div>
-      <h1>BOOK CLUB </h1>
+ <h1> BOOK CLUB </h1>
+<img alt= '' src={window.location.origin + '/book.jpg' } />
+ <br></br>
+ <br></br>
+<button onClick={this.allBooks}> All Books </button>
+<button> My Books </button>
     </div>
-  );
-}
+    )}
+
+  }
+
 
 export default App;
