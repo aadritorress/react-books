ffeda916697f509df3e566e5ab548e62655ba9b2
Adriana <adriana.amaral@msn.com>
2021-07-07 17:00:24 -0500

    button-delete

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -19,6 +19,7 @@ handleClick (e) {
         <h2>{this.props.book.name}</h2>
 
         <button onClick={() => this.props.manageBooks(this.props.book)} className="favorite-button"> click here to add or remove book </button>
+        <button className="favorite-button"> delete book </button>
       </div>
     );
   }
